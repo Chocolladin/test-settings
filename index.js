9f92f8e186289ad1fb192881f9d3f096a1bcfc2d
Chocolladin <bertrand.zuel@gmail.com>
2021-03-19 17:14:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var nconf = require('nconf');
 var path = require('path');
 
 nconf.env().argv();
-var env = nconf.get('NODE_ENV') || 'production';
+var env = nconf.get('NODE_ENV') || 'dev';
 nconf.file(path.join(__dirname, env + '.json'));
 var settings = nconf.get();
 debug(settings);
